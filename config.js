5394612e48fc645f044dbdfd33c9b232ff2cf43f
Jonathan Baez <jbaez9910@gmail.com>
2021-06-26 14:56:14 -0500

    conexion a bd mysql

--- config.js
+++ config.js
@@ -4,5 +4,11 @@ module.exports={
     },
     jwt:{
         secret: process.env.JWT_SECRET || 'notasecreta!',
-    }
+    },
+    mysql:{
+        host : process.env.MYSQL_HOST || 'remotemysql.com',
+        user : process.env.MYSQL_USER || 'oFGULJUYbu',
+        password : process.env.MYSQL_PASSWORD || 'BUej0zeKVu',
+        database : process.env.MYSQL_DATABASE || 'oFGULJUYbu',
+    },
 }
